6eef994f2b2f0aec60c2c1f5abe42874468c6f4c
Kyou13 <kyou1013@gmail.com>
2018-04-16 19:18:02 +0900

    add css js

---
+++ js/style.js
@@ -0,0 +1,11 @@
+// 背景
+$('#bg').particleground({ //要素の指定
+    dotColor:  '#7dfcfd', //ドットの色
+    lineColor: '#7dfcfd',
+    density: 6500,
+    minSpeedX: 0.2,
+    particleRadius: 5,
+    parallax: false,
+    maxSpeedX: 0.1,
+    maxSpeedY: 0.1,
+});
