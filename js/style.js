9b146792aefc4f5e3a8282c1e42c7e6f2f6a6c35
Kyou13 <kyou1013@gmail.com>
2018-04-18 01:35:05 +0900

    fix js scroll

--- js/style.js
+++ js/style.js
@@ -9,3 +9,12 @@ $('#bg').particleground({ //要素の指定
     maxSpeedX: 0.1,
     maxSpeedY: 0.1,
 });
+
+$('a[href^="#"]').click(function() {
+   var speed = 400;
+   var href= $(this).attr("href");
+   var target = $(href == "#" || href == "" ? 'html' : href);
+   var position = target.offset().top;
+   $('body,html').animate({scrollTop:position}, speed, 'swing');
+   return false;
+});
